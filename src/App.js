6ff6ca03d58f5e79121e6e1c0804d1015dc8268b
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2023-12-12 07:36:16 +0530

    setup tailwind

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './components/Button/Button';
+import HelloWorld from './components/HelloWorld/HelloWorld';
 
 function App() {
-  return (
-    <div className="App">
-      React by LWD
-    </div>
-  );
+    return (
+        <div className='container mx-auto'>
+            <Button />
+            <Button />
+
+            <HelloWorld />
+        </div>
+    );
 }
 
 export default App;
