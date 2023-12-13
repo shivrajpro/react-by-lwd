e329ab25dac931c072fad8b86acde80f9ff166cf
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2023-12-13 08:03:49 +0530

    coding exercise 11 solution

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
+import Todo from './Todo'
 
-function App() {
+// don't remove the export keyword here!
+export const DUMMY_TODOS = [
+    'Learn React',
+    'Practice React',
+    'Profit!'
+];
+
+// don't change the Component name "App"
+export default function App() {
     return (
-        <div className='container mx-auto'>
-            <h2>app works!</h2>
+        <div>
+            {DUMMY_TODOS.map((todo, index) => {
+                return (<Todo key={index} text={todo} />)
+            })}
         </div>
-    );
+    )
 }
-
-export default App;
