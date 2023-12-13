950668f45949cde0ba73673e9ec89f4278a07b13
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2023-12-13 07:31:40 +0530

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import './App.css';
-import Button from './components/Button/Button';
-import HelloWorld from './components/HelloWorld/HelloWorld';
-import Post from './components/Posts/Posts';
 
 function App() {
     return (
         <div className='container mx-auto'>
-            <Post/>
-            {/* <Button name="First Button" />
-            <Button name="Second Button" />
-            <HelloWorld text="Good Morning" /> */}
+            <h2>app works!</h2>
         </div>
     );
 }
