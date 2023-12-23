4a40e2a8fdd075df24be867ca6f4c29cb2a19b0b
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2023-12-23 13:48:31 +0530

    useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import './App.css';
 import Counter from './components/Counter/Counter';
 
 function App() {
+    const [showCounter, setShowCounter] = useState(true);
+
     return (
         <div className='m-2 p-2 border border-gray-500 container mx-auto'>
             <p>app works!</p>
-            <Counter/>
+            <div> 
+                <button className='mx-2 bg-green-500 px-3 py-1 text-white'
+                onClick={()=> setShowCounter(!showCounter)}
+                >
+                    Toggle Component    
+                </button> 
+            </div>
+            <div> {showCounter && <Counter/>} </div>
         </div>
     );
 }
