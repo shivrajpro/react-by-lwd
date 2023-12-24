7bb2aea10f62ad305421a5b10d3564d0d5724c01
shivrajhulbatte@gmail.com <shivrajhulbatte@gmail.com>
2023-12-24 15:58:14 +0530

    cleanup

--- src/axiosinstance.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL:'https://legacy-react-v17-default-rtdb.firebaseio.com'
-});
-
-axios.defaults.headers.common['Authorization'] = '[instance] auth token';
-
-export default instance;
