6038df6e72b1303b7c6f2d6ff37fe6c1833bb79c
shivrajpro <shivrajhulbatte@gmail.com>
2024-02-20 17:38:17 +0530

    center align sign in and sign up page content

--- src/Pages/SignIn/SignIn.js
+++ src/Pages/SignIn/SignIn.js
@@ -1,95 +1,101 @@
 import { useState } from "react";
 import { connect, useDispatch } from "react-redux";
 import Loader from "../../components/Loader/Loader";
-import { signInAction, toggleLoadingAction } from "../../store/actions/AuthActions";
+import {
+  signInAction,
+  toggleLoadingAction,
+} from "../../store/actions/AuthActions";
 
 function SignIn(props) {
-    // console.log('SignIn', props);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  // console.log('SignIn', props);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    let errorObj = { email:'', password: '' };
-    const [errors, setErrors] = useState(errorObj);
+  let errorObj = { email: "", password: "" };
+  const [errors, setErrors] = useState(errorObj);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    function onSignIn(e) {
-        e.preventDefault();
-        const errorObj = {email:'', password: ''};
+  function onSignIn(e) {
+    e.preventDefault();
+    const errorObj = { email: "", password: "" };
 
-        let hasError = false;
-        if(email === ''){
-            errorObj.email = 'email id is required';
-            hasError = true;
-        }
-        if(password === ''){
-            errorObj.password = 'password is required';
-            hasError = true;
-        }
-
-        setErrors(errorObj);
-        if(hasError) return;
-
-        // console.log('form value',email, password);
-        dispatch(toggleLoadingAction(true));
-        dispatch(signInAction(email, password, props.history));
+    let hasError = false;
+    if (email === "") {
+      errorObj.email = "email id is required";
+      hasError = true;
     }
-    return (
-        <div className="p-2">
-            <p>sign in works!</p>
+    if (password === "") {
+      errorObj.password = "password is required";
+      hasError = true;
+    }
+
+    setErrors(errorObj);
+    if (hasError) return;
+
+    // console.log('form value',email, password);
+    dispatch(toggleLoadingAction(true));
+    dispatch(signInAction(email, password, props.history));
+  }
+  return (
+    <div className="flex justify-center my-5">
+      {/* <p>sign in works!</p> */}
+      <div>{props.showLoader && <Loader />}</div>
+      <div>
+        {" "}
+        {props.successMsg && (
+          <p className="bg-green-500 text-white p-2">sign in successful</p>
+        )}
+      </div>
+      <div>
+        {" "}
+        {props.errorMsg && (
+          <p className="bg-red-500 text-white p-2">{props.errorMsg}</p>
+        )}
+      </div>
+      <div className="my-2">
+        <form onSubmit={onSignIn}>
+          <div className="my-2">
+            <label>Email</label>
             <div>
-                {props.showLoader && <Loader/>}
+              <input
+                type="text"
+                className="my-2 p-2 border border-gray-500"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-            <div> {props.successMsg && 
-                <p className="bg-green-500 text-white p-2" >sign in successful</p>} 
+            <div className="text-red-600">{errors.email}</div>
+          </div>
+          <div className="my-2">
+            <label>Password</label>
+            <div>
+              <input
+                type="password"
+                className="my-2 p-2 border border-gray-500"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
             </div>
-            <div> {props.errorMsg && 
-                <p className="bg-red-500 text-white p-2" >{props.errorMsg}</p>} 
-            </div>
-            <div className="my-2" >
-                <form onSubmit={onSignIn}>
-                    <div className="my-2" >
-                        <label>Email</label>
-                        <div>
-                            <input type="text" 
-                            className="my-2 p-2 border border-gray-500" 
-                            value={email}
-                            onChange={(e)=> setEmail(e.target.value)}
-                            />
-                        </div>
-                        <div className="text-red-600" >
-                            {errors.email}
-                        </div>
-                    </div>
-                    <div className="my-2">
-                        <label>Password</label>
-                        <div>
-                            <input type="password" 
-                            className="my-2 p-2 border border-gray-500" 
-                            value={password}
-                            onChange={(e)=> setPassword(e.target.value)}
-                            />
-                        </div>
-                        <div className="text-red-600" >
-                            {errors.password}
-                        </div>
-                    </div>
-                    <div>
-                        <button type="submit" 
-                        className="bg-green-500 p-2 text-white" >Sign In</button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
+            <div className="text-red-600">{errors.password}</div>
+          </div>
+          <div>
+            <button type="submit" className="bg-green-500 p-2 text-white">
+              Sign In
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-const makeStateToProps = (state)=>{
-    // console.log('makeStateToProps', state);
-    return {
-        errorMsg: state.auth.errorMessage,
-        successMsg: state.auth.successMessage,
-        showLoader: state.auth.showLoading
-    }
-}
+const makeStateToProps = (state) => {
+  // console.log('makeStateToProps', state);
+  return {
+    errorMsg: state.auth.errorMessage,
+    successMsg: state.auth.successMessage,
+    showLoader: state.auth.showLoading,
+  };
+};
 export default connect(makeStateToProps)(SignIn);
--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,95 +1,96 @@
 import { useState } from "react";
 import { connect, useDispatch } from "react-redux";
 import Loader from "../../components/Loader/Loader";
-import { signUpAction, toggleLoadingAction } from "../../store/actions/AuthActions";
+import {
+  signUpAction,
+  toggleLoadingAction,
+} from "../../store/actions/AuthActions";
 
 function SignUp(props) {
-    // console.log('SignUp', props);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    let initErrors = { email: '', password: '' };
-    const [errors, setErrors] = useState(initErrors);
+  // console.log('SignUp', props);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let initErrors = { email: "", password: "" };
+  const [errors, setErrors] = useState(initErrors);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    function onSignUp(e) {
-        e.preventDefault();
-        let hasError = false;
+  function onSignUp(e) {
+    e.preventDefault();
+    let hasError = false;
 
-        const errorObj = { ...errors };
-        if (email === '') {
-            errorObj.email = 'email is required';
+    const errorObj = { ...errors };
+    if (email === "") {
+      errorObj.email = "email is required";
 
-            hasError = true;
-        }
-        if (password === '') {
-            errorObj.password = 'password is required';
-
-            hasError = true;
-        }
-
-        setErrors(errorObj);
-        if (hasError) return;
-
-        dispatch(toggleLoadingAction(true));
-        dispatch(signUpAction(email, password, props.history));
+      hasError = true;
     }
-    return (
-        <div className="p-2">
-            <p>singup works!</p>
-            {props.showLoader && <Loader/>}
-            <h1 className="text-2xl font-extrabold" >Sign Up Form</h1>
-            {props.errorMsg && <div className="p-2 bg-red-500 text-white">
-                {props.errorMsg}
-            </div>}
-            {props.successMsg && <div className="p-2 bg-green-500 text-white">
-                {props.successMsg}
-            </div>}
-            <div>
-                <form onSubmit={onSignUp} >
-                    <div>
-                        <label>Email Id</label>
-                        <div>
-                            <input type="email"
-                                className="p-2 my-2 border border-gray-500"
-                                value={email}
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                        <div className="text-red-900" >
-                            {errors.email}
-                        </div>
-                    </div>
-                    <div>
-                        <label>Password</label>
-                        <div>
-                            <input type="password"
-                                className="p-2 my-2 border border-gray-500"
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                            />
-                        </div>
-                        <div className="text-red-900" >
-                            {errors.password}
-                        </div>
+    if (password === "") {
+      errorObj.password = "password is required";
 
-                    </div>
-                    <div>
-                        <button type="submit"
-                            className="p-2 my-2 text-white bg-green-500" >SignUp</button>
-                    </div>
-                </form>
+      hasError = true;
+    }
+
+    setErrors(errorObj);
+    if (hasError) return;
+
+    dispatch(toggleLoadingAction(true));
+    dispatch(signUpAction(email, password, props.history));
+  }
+  return (
+    <div className="flex justify-center my-5">
+      {props.showLoader && <Loader />}
+      {props.errorMsg && (
+        <div className="p-2 bg-red-500 text-white">{props.errorMsg}</div>
+      )}
+      {props.successMsg && (
+        <div className="p-2 bg-green-500 text-white">{props.successMsg}</div>
+      )}
+      <div>
+        <h1 className="text-2xl font-extrabold">Sign Up Form</h1>
+
+        <form onSubmit={onSignUp}>
+          <div>
+            <label>Email Id</label>
+            <div>
+              <input
+                type="email"
+                className="p-2 my-2 border border-gray-500"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-        </div>
-    )
+            <div className="text-red-900">{errors.email}</div>
+          </div>
+          <div>
+            <label>Password</label>
+            <div>
+              <input
+                type="password"
+                className="p-2 my-2 border border-gray-500"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="text-red-900">{errors.password}</div>
+          </div>
+          <div>
+            <button type="submit" className="p-2 my-2 text-white bg-green-500">
+              SignUp
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 const mapStateToProps = (state) => {
-    return {
-        errorMsg: state.auth.errorMessage,
-        successMsg: state.auth.successMessage,
-        showLoader: state.auth.showLoading
-    }
-}
+  return {
+    errorMsg: state.auth.errorMessage,
+    successMsg: state.auth.successMessage,
+    showLoader: state.auth.showLoading,
+  };
+};
 
 export default connect(mapStateToProps)(SignUp);
