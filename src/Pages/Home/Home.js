81a350b52670eca6a0ddc5af5c02fa5ee1a2a250
shivrajpro <shivrajhulbatte@gmail.com>
2024-02-21 17:10:03 +0530

    add content in home page

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,7 +1,24 @@
 export default function Home() {
-    return (
-        <div className="p-2">
-            <p>home works!</p>
-        </div>
-    )
+  return (
+    <div className="flex justify-center">
+      <div className="p-2">
+        <h1 className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+          Purpose of this application
+        </h1>
+        <p className="text-xl my-2 font-bold">
+          To explore the below concepts in ReactJS v17{" "}
+        </p>
+        <ul className="list-disc list-inside">
+          <li className="mb-2">React functional and class components</li>
+          <li className="mb-2">Virtual DOM</li>
+          <li className="mb-2">Props and prop-types</li>
+          <li className="mb-2">React Hooks</li>
+          <li className="mb-2">Component LifeCycle</li>
+          <li className="mb-2">React Router</li>
+          <li className="mb-2">Redux</li>
+          <li className="mb-2">Deployment with firebase</li>
+        </ul>
+      </div>
+    </div>
+  );
 }
--- src/Pages/SinglePost/SinglePost.js
+++ src/Pages/SinglePost/SinglePost.js
@@ -2,36 +2,36 @@ import { connect } from "react-redux";
 import { getPost } from "../../store/selectors/PostSelectors";
 
 function SinglePost(props) {
-    // console.log('props',props);
-    return (
+  // console.log('props',props);
+  return (
+    <div className="border border-gray-600 p-2">
+      <p>single post works!</p>
+      {props.post && (
         <div>
-            <p>single post works!</p>
-            {props.post &&
-                <div>
-                    <p>
-                        <span className="font-bold" >Id : </span>
-                        {props.post.id}
-                    </p>
-                    <p>
-                        <span className="font-bold" >Title : </span>
-                        {props.post.title}
-                    </p>
-                    <p>
-                        <span className="font-bold" >Description : </span>
-                        {props.post.description}
-                    </p>
-                </div>
-            }
+          <p>
+            <span className="font-bold">Id : </span>
+            {props.post.id}
+          </p>
+          <p>
+            <span className="font-bold">Title : </span>
+            {props.post.title}
+          </p>
+          <p>
+            <span className="font-bold">Description : </span>
+            {props.post.description}
+          </p>
         </div>
-    )
+      )}
+    </div>
+  );
 }
 
 const makeStateToProps = () => {
-    const getPostDetails = getPost();
-    return (state, props) => {
-        return {
-            post: getPostDetails(state, props.match.params.id)
-        }
-    }
-}
+  const getPostDetails = getPost();
+  return (state, props) => {
+    return {
+      post: getPostDetails(state, props.match.params.id),
+    };
+  };
+};
 export default connect(makeStateToProps)(SinglePost);
